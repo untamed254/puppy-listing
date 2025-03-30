104b5c98ca2d466eee8e302fd513ebec151d95d4
untamed254 <collokim680@gmail.com>
2025-03-30 16:35:10 +0300

    add-breed page

--- Admin/add-breed.php
+++ Admin/add-breed.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel= "stylesheet" href="add-pet.css">
+    <title>Breeds</title>
 </head>
 <body>
-    <!-- breed name  -->
+    <div class="registration-form">
+        <div class="form-header">
+            <h3 class="form-title">Add Breed</h3>
+        </div>
+        <div class="form-body">
+            <form>
+                <div class="mb-3">
+                    <label for="Breed" class="form-label">Breed</label>
+                    <input type="Text" class="form-control" id="Breed" required>
+                </div>
+                <button type="submit" class="btn submit-btn">Add Breed</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
