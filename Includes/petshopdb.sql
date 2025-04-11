8078b23002c262ce8e24d13378c92db5701532d8
Dunkoh <sirduncan887@gmail.com>
2025-04-11 20:02:27 +0300

    added updated db

--- Includes/petshopdb.sql
+++ Includes/petshopdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2025 at 11:17 PM
+-- Generation Time: Apr 11, 2025 at 07:01 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -41,7 +41,7 @@ CREATE TABLE `admin_table` (
 
 INSERT INTO `admin_table` (`admin_id`, `admin_name`, `admin_email`, `admin_image`, `admin_password`) VALUES
 (1, 'Duncan', ' duncan@petpawa.info', 'img1.jpg', '$2y$10$9jvYQ97O0V5JajkbtYMLSeWTcP65Gku0OEheu.g/p8gIfdaoDAsqm'),
-(2, 'Mwas', ' duncan@petpawa.info', '', '$2y$10$nGlBzElIxROX5KfjsEkMHeOtO/w8sR09UfEw.w8Vfz1GUB84duuw2');
+(2, 'Mwas', 'duncan@petpawa.info', '', '$2y$10$nGlBzElIxROX5KfjsEkMHeOtO/w8sR09UfEw.w8Vfz1GUB84duuw2');
 
 -- --------------------------------------------------------
 
@@ -61,7 +61,20 @@ CREATE TABLE `pet_category` (
 
 INSERT INTO `pet_category` (`category_id`, `category_title`, `created_at`) VALUES
 (1, 'Dog', '2025-04-04 20:40:15'),
-(5, 'Cat', '2025-04-04 21:09:32');
+(5, 'Cat', '2025-04-04 21:09:32'),
+(6, 'Monkey', '2025-04-09 19:35:13');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pet_images`
+--
+
+CREATE TABLE `pet_images` (
+  `image_id` int(11) NOT NULL,
+  `puppy_id` int(11) NOT NULL,
+  `image_url` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -71,8 +84,21 @@ INSERT INTO `pet_category` (`category_id`, `category_title`, `created_at`) VALUE
 
 CREATE TABLE `puppy_breed` (
   `breed_id` int(11) NOT NULL,
-  `breed_name` int(11) NOT NULL
+  `breed_name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `puppy_breed`
+--
+
+INSERT INTO `puppy_breed` (`breed_id`, `breed_name`) VALUES
+(1, 'German Shepherd'),
+(2, 'Bulldog'),
+(3, 'Poodle'),
+(4, 'Beagle'),
+(5, 'Siamese'),
+(6, 'Persian'),
+(7, 'Maine Coon');
 
 -- --------------------------------------------------------
 
@@ -82,13 +108,23 @@ CREATE TABLE `puppy_breed` (
 
 CREATE TABLE `puppy_listing` (
   `puppy_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `breed_id` int(11) NOT NULL,
   `puppy_name` varchar(100) NOT NULL,
-  `puppy_breed` varchar(100) NOT NULL,
   `puppy_age` int(11) NOT NULL,
   `puppy_location` varchar(100) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
   `puppy_desc` varchar(255) NOT NULL,
-  `puppy_img` varchar(255) NOT NULL
+  `puppy_img` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `puppy_listing`
+--
+
+INSERT INTO `puppy_listing` (`puppy_id`, `category_id`, `breed_id`, `puppy_name`, `puppy_age`, `puppy_location`, `price`, `puppy_desc`, `puppy_img`, `created_at`) VALUES
+(1, 1, 1, 'Max', 3, 'Nairobi', '25000.00', 'Purebred German Shepherd puppy with vaccinations', 'german-shepherd.jpg', '2025-04-11 15:03:17');
 
 --
 -- Indexes for dumped tables
@@ -107,10 +143,26 @@ ALTER TABLE `pet_category`
   ADD PRIMARY KEY (`category_id`);
 
 --
+-- Indexes for table `pet_images`
+--
+ALTER TABLE `pet_images`
+  ADD PRIMARY KEY (`image_id`),
+  ADD KEY `puppy_id` (`puppy_id`);
+
+--
+-- Indexes for table `puppy_breed`
+--
+ALTER TABLE `puppy_breed`
+  ADD PRIMARY KEY (`breed_id`);
+
+--
 -- Indexes for table `puppy_listing`
 --
 ALTER TABLE `puppy_listing`
-  ADD PRIMARY KEY (`puppy_id`);
+  ADD PRIMARY KEY (`puppy_id`),
+  ADD KEY `idx_category` (`category_id`),
+  ADD KEY `idx_breed` (`breed_id`),
+  ADD KEY `idx_price` (`price`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -126,13 +178,43 @@ ALTER TABLE `admin_table`
 -- AUTO_INCREMENT for table `pet_category`
 --
 ALTER TABLE `pet_category`
-  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `pet_images`
+--
+ALTER TABLE `pet_images`
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `puppy_breed`
+--
+ALTER TABLE `puppy_breed`
+  MODIFY `breed_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `puppy_listing`
 --
 ALTER TABLE `puppy_listing`
-  MODIFY `puppy_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `puppy_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `pet_images`
+--
+ALTER TABLE `pet_images`
+  ADD CONSTRAINT `pet_images_ibfk_1` FOREIGN KEY (`puppy_id`) REFERENCES `puppy_listing` (`puppy_id`);
+
+--
+-- Constraints for table `puppy_listing`
+--
+ALTER TABLE `puppy_listing`
+  ADD CONSTRAINT `puppy_listing_ibfk_2` FOREIGN KEY (`breed_id`) REFERENCES `puppy_breed` (`breed_id`),
+  ADD CONSTRAINT `puppy_listing_ibfk_3` FOREIGN KEY (`category_id`) REFERENCES `pet_category` (`category_id`),
+  ADD CONSTRAINT `puppy_listing_ibfk_4` FOREIGN KEY (`breed_id`) REFERENCES `puppy_breed` (`breed_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
